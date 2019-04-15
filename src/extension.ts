a72dd4aa35f0d59772118f89e8d87766da5cce40
Danny Tuppeny <danny@tuppeny.com>
2019-04-15 08:42:31 +0100

    Make basic extension

---
+++ src/extension.ts
@@ -0,0 +1,8 @@
+import * as fs from "fs";
+import * as path from "path";
+import { window, workspace, ExtensionContext, Uri, commands, version, languages, TextDocument, TextEdit, Range, Position } from "vscode";
+
+export function activate(context: ExtensionContext) {
+}
+
+export function deactivate() { }
