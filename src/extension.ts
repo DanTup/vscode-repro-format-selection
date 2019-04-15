85d4b0f4c6f08f20486c9f9155b75ac26b0fbe2a
Danny Tuppeny <danny@tuppeny.com>
2019-04-15 08:42:59 +0100

    Make formatter that strips multiple newlines

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,17 @@ import * as path from "path";
 import { window, workspace, ExtensionContext, Uri, commands, version, languages, TextDocument, TextEdit, Range, Position } from "vscode";
 
 export function activate(context: ExtensionContext) {
+	languages.registerDocumentFormattingEditProvider({ language: "foo" }, {
+		async provideDocumentFormattingEdits(document: TextDocument, options, token): Promise<TextEdit[]> {
+			const origText = document.getText();
+			return [
+				new TextEdit(
+					new Range(new Position(0, 0), document.positionAt(origText.length)),
+					origText.replace(/\n{2,}/g, '\n'),
+				),
+			];
+		},
+	});
 }
 
 export function deactivate() { }
